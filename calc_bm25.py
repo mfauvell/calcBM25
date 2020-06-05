de4c720237487161a2f84fffe35bfc7280f1929f
mfauvell <mfauvell@dismental.org>
2020-06-05 08:46:44 +0200

    Arguments processed and gitignore changed to ignore data and result dirs

--- calc_bm25.py
+++ calc_bm25.py
@@ -5,27 +5,32 @@
 
 import sys
 import getopt
+import csv
 
 
 def process_arguments(argv):
     '''Process script arguments and check if there an error'''
 
+    help_text = 'Help here'
     tweet_file = ''
     queries_file = ''
     frecuencies_file = ''
-    destination_file = ''
+    local_entity = ''
+    destination_dir = 'result'
     local_k = ''
     local_k1 = ''
     local_k2 = ''
     try:
-        opts, _ = getopt.getopt(argv, "ht:q:f:d:",
-                                ["--tweetFile", "--queriesFile", "--frecuenciesFile", "--destinationFile", "--k1", "--k2", "--K"])
+        opts, _ = getopt.getopt(argv, "ht:q:f:d:e:",
+                                ["tweetFile=", "queriesFile=", "frecuenciesFile=", "destinationDir=", "entity=", "k1=", "k2=", "K="])
     except getopt.GetoptError:
-        print('Help here')
+        print(help_text)
         sys.exit(2)
+    print(opts)
     for opt, arg in opts:
+        print(opt)
         if opt == '-h':
-            print('Help here')
+            print(help_text)
             sys.exit()
         elif opt in ("-t", "--tweetFile"):
             tweet_file = arg
@@ -33,8 +38,20 @@ def process_arguments(argv):
             queries_file = arg
         elif opt in ("-f", "--frecuenciesFile"):
             frecuencies_file = arg
-    # TODO Tratar el retorno para que si no están los obligatorios pete.
-    return tweet_file, queries_file, frecuencies_file, destination_file, local_k, local_k1, local_k2
+        elif opt in ("-d", "--destinationDir"):
+            destination_dir = arg
+        elif opt in ("-e", "--entity"):
+            local_entity = arg
+        elif opt == "--K":
+            local_k = arg
+        elif opt == "--k1":
+            local_k1 = arg
+        elif opt == "--k2":
+            local_k2 = arg
+    if (tweet_file == '' or queries_file == '' or frecuencies_file == ''):
+        print(help_text)
+        sys.exit(2)
+    return tweet_file, queries_file, frecuencies_file, destination_dir, local_entity, local_k, local_k1, local_k2
 
 
-tweetFile, queriesFile, frecuenciesFile, destinationFile, k, k1, k2 = process_arguments(sys.argv[1:])
+tweetFile, queriesFile, frecuenciesFile, destinationDir, entity, k, k1, k2 = process_arguments(sys.argv[1:])
